37b3465404358143cb364e25de7c189fadc3f59c
Kate <YarmoshkoPTI08031986>
2022-07-25 17:51:10 +0300

    ДЗ по уроку №5

---
+++ src/lesson05/HomeWorkPart2.java
@@ -0,0 +1,11 @@
+package lesson05;
+
+public class HomeWorkPart2 {
+    public static void main(String[] args) {
+        /*
+        Умножение двух матриц. Создайте два массива целых чисел размером 3х3 (две матрицы). Напишите
+программу для умножения двух матриц.
+         */
+//        int [][] =
+    }
+}
