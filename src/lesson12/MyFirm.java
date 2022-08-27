a5db8d9e86b3910783d7aeaeeb9ef031e280a0f3
Kate <YarmoshkoPTI08031986>
2022-08-28 01:09:12 +0300

    Дз урок № 12.

---
+++ src/lesson12/MyFirm.java
@@ -0,0 +1,10 @@
+package lesson12;
+
+public class MyFirm {
+    public static void main(String[] args) {
+        Accounting accounting = new Accounting();
+        accounting.addIdCardOne();
+        accounting.addIdCardTwo();
+        accounting.printReport();
+    }
+}
